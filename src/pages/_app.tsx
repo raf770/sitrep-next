8827c4d91576fbdb69c8f84829f82daa98c2bcf8
Raphaël PARIENTE <raphaelpariente@macbookair-4.home>
2026-04-05 21:03:35 +0200

    Add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from '@vercel/analytics/react'
 import '../app/globals.css'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   )
 }
