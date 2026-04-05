1fa9bbc736ef5885cf129014560a2ee0a3358e67
Raphaël PARIENTE <raphaelpariente@macbookair-4.home>
2026-04-05 13:33:12 +0200

    Remove conflicting admin page

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect } from "react";
-
-export default function AdminPage() {
-  useEffect(() => {
-    window.location.href = "/admin/index.html";
-  }, []);
-  return null;
-}
